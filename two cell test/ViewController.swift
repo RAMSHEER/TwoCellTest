347571e06f2def15387ddefa3f22947f58c042c4
Rupesh E <rupeshe2010@gmail.com>
2020-03-06 10:31:39 +0530

    Initial Commit

---
+++ two cell test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  two cell test
+//
+//  Created by Purpose Code on 06/03/20.
+//  Copyright © 2020 EmojiView. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ two cell testTests/two_cell_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  two_cell_testTests.swift
+//  two cell testTests
+//
+//  Created by Purpose Code on 06/03/20.
+//  Copyright © 2020 EmojiView. All rights reserved.
+//
+
+import XCTest
+@testable import two_cell_test
+
+class two_cell_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
