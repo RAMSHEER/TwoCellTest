f19d637ae0e1ad008c35a329c0b09ab7e51f66c8
Rupesh E <rupeshe2010@gmail.com>
2020-03-12 18:21:57 +0530

    first commit

---
+++ two cell test/ReplyCelll.swift
@@ -0,0 +1,24 @@
+//
+//  ReplyCelll.swift
+//  two cell test
+//
+//  Created by Purpose Code on 06/03/20.
+//  Copyright © 2020 EmojiView. All rights reserved.
+//
+
+import UIKit
+
+class ReplyCelll: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
