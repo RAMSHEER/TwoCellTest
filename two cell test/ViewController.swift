06e8c26819517e460c5203d85baf018d7d57b465
Rupesh E <rupeshe2010@gmail.com>
2020-03-12 18:23:07 +0530

    uploading all

--- two cell test/ViewController.swift
+++ two cell test/ViewController.swift
@@ -17,4 +17,45 @@ class ViewController: UIViewController {
 
 
 }
+extension ViewController: UITableViewDelegate,UITableViewDataSource{
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return  UITableView.automaticDimension
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 5
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+            return 5
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+                   let cell: ReplyCelll  = tableView.dequeueReusableCell(withIdentifier: "ReplyCelll", for: indexPath) as! ReplyCelll
+             return cell
+        
+        
+        
+ 
+    }
+    
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+                 let cell: CommentCell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            return cell
+        
+        
+    }
+        
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    
+}
 
